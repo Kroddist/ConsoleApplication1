ee84ae01a87d0a1a455ba2688d7849cc9d72914c
Kroddist <razvan.slivca26@gmail.com>
2025-03-31 13:59:13 +0300

    Add files via upload

--- 10.03.2025.cs
+++ 10.03.2025.cs
@@ -1,44 +1,49 @@
 using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 class Program
 {
     static void Main()
     {
-        int[,] matrix = {
-            { 1, -2, 0 },
-            { -4, 5, -6 },
-            { 0, 8, 9 }
-        };
-        int positiveCount = 0, negativeCount = 0;
-        Console.WriteLine("Координаты нулевых элементов:");
-        for (int i = 0; i < matrix.GetLength(0); i++)
-        {
-            for (int j = 0; j < matrix.GetLength(1); j++)
-            {
-                if (matrix[i, j] > 0) positiveCount++;
-                else if (matrix[i, j] < 0) negativeCount++;
-                else Console.WriteLine($"({i}, {j})");
-            }
-        }
-        Console.WriteLine($"Положительных: {positiveCount}, Отрицательных: {negativeCount}\n");
+        string s1 = "программирование";
+        string s2 = new string(s1.Where((c, i) => i % 2 == 0).ToArray());
+        Console.WriteLine("Слово s2: " + s2);
         
-        string firstWord = "Программирование";
-        string secondWord = "Код";
-        secondWord = firstWord.Substring(0, secondWord.Length);
-        Console.WriteLine("Новое второе слово: " + secondWord + "\n");
+        string text = "Тест + текст + с * разными * символами+*+";
+        int plusCount = text.Count(c => c == '+');
+        int starCount = text.Count(c => c == '*');
+        Console.WriteLine($"Символ '+' встречается {plusCount} раз, символ '*' - {starCount} раз");
         
-        string sentence = "Анна написала длинное сочинение.";
-        var nnMatches = System.Text.RegularExpressions.Regex.Matches(sentence, "нн");
-        Console.WriteLine("Буквосочетания 'нн' найдены: " + nnMatches.Count + " раз\n");
+        string sentence = "Это пример, текста с запятой.";
+        int commaIndex = sentence.IndexOf(',');
+        if (commaIndex != -1)
+            Console.WriteLine("До первой запятой: " + sentence.Substring(0, commaIndex));
+        else
+            Console.WriteLine("Запятых нет");
         
-        string text = "Текст с разными буквами";
-        int lastS = text.LastIndexOf('с');
-        int lastT = text.LastIndexOf('Т');
-        Console.WriteLine(lastS > lastT ? "'с' встречается позже." : "'Т' встречается позже." + "\n");
+        string sentence2 = "  Образование очень важно.".Trim();
+        int firstSpace = sentence2.IndexOf(' ');
+        string firstWord = firstSpace == -1 ? sentence2 : sentence2.Substring(0, firstSpace);
+        int oCount = firstWord.Count(c => c == 'о' || c == 'О');
+        Console.WriteLine("Количество 'о' в первом слове: " + oCount);
         
-        string checkText = "ааааабвгд";
-        bool hasFiveInRow = System.Text.RegularExpressions.Regex.IsMatch(checkText, "(.)\\1{4}");
-        Console.WriteLine(hasFiveInRow ? "Есть пять подряд одинаковых символов." : "Нет пяти подряд одинаковых символов.");
+        string paragraph = "Это первое предложение. А это второе! И третье?";
+        int sentenceCount = Regex.Matches(paragraph, "[.!?]").Count;
+        Console.WriteLine("Количество предложений: " + sentenceCount);
+        
+        string sentence3 = "Летнее небо очень красивое.";
+        string replacedE = sentence3.Replace('е', 'и');
+        Console.WriteLine("Замененный текст: " + replacedE);
+        
+        string sentence4 = "Махаон - красивый бабочка.";
+        string replacedAx = sentence4.Replace("ах", "ух");
+        Console.WriteLine("Измененный текст: " + replacedAx);
+        
+        string word = "программирование";
+        char[] wordArray = word.ToCharArray();
+        Array.Reverse(wordArray, 2, 8);
+        string newWord = new string(wordArray);
+        Console.WriteLine("Новое слово: " + newWord);
     }
 }

3e8d5d65b7e94cc8845629a713da6e5a35167dc4
Kroddist <razvan.slivca26@gmail.com>
2025-04-02 11:52:55 +0300

    Add files via upload

--- 27.03.2025.cs
+++ 27.03.2025.cs
@@ -1,76 +1,179 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
 
-class Journal
+namespace BankSystem
 {
-    public int Employees { get; set; }
-    public Journal(int employees) => Employees = employees;
-    public static Journal operator +(Journal j, int value) => new Journal(j.Employees + value);
-    public static Journal operator -(Journal j, int value) => new Journal(j.Employees - value);
-    public static bool operator ==(Journal j1, Journal j2) => j1.Employees == j2.Employees;
-    public static bool operator !=(Journal j1, Journal j2) => j1.Employees != j2.Employees;
-    public static bool operator <(Journal j1, Journal j2) => j1.Employees < j2.Employees;
-    public static bool operator >(Journal j1, Journal j2) => j1.Employees > j2.Employees;
-    public override bool Equals(object obj) => obj is Journal j && Employees == j.Employees;
-    public override int GetHashCode() => Employees.GetHashCode();
+    public enum AccountType { Savings, Current }
+
+    public class BankAccount
+    {
+        private static int accountCounter = 1000;
+        private int accountNumber;
+        private double balance;
+        private AccountType accountType;
+
+        public BankAccount(AccountType type, double initialBalance)
+        {
+            this.accountNumber = ++accountCounter;
+            this.accountType = type;
+            this.balance = initialBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0) balance += amount;
+        }
+
+        public bool Withdraw(double amount)
+        {
+            if (amount > balance) return false;
+            balance -= amount;
+            return true;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Account Number: {accountNumber}, Type: {accountType}, Balance: {balance}");
+        }
+    }
 }
 
-class Shop
+namespace SortingAlgorithms
 {
-    public double Area { get; set; }
-    public Shop(double area) => Area = area;
-    public static Shop operator +(Shop s, double value) => new Shop(s.Area + value);
-    public static Shop operator -(Shop s, double value) => new Shop(s.Area - value);
-    public static bool operator ==(Shop s1, Shop s2) => s1.Area == s2.Area;
-    public static bool operator !=(Shop s1, Shop s2) => s1.Area != s2.Area;
-    public static bool operator <(Shop s1, Shop s2) => s1.Area < s2.Area;
-    public static bool operator >(Shop s1, Shop s2) => s1.Area > s2.Area;
-    public override bool Equals(object obj) => obj is Shop s && Area == s.Area;
-    public override int GetHashCode() => Area.GetHashCode();
+    public class Sort
+    {
+        public static void BubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (arr[j] > arr[j + 1])
+                        (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+        }
+
+        public static void QuickSort(int[] arr, int left, int right)
+        {
+            if (left >= right) return;
+            int pivot = arr[right], i = left;
+            for (int j = left; j < right; j++)
+                if (arr[j] < pivot)
+                    (arr[i++], arr[j]) = (arr[j], arr[i]);
+            (arr[i], arr[right]) = (arr[right], arr[i]);
+            QuickSort(arr, left, i - 1);
+            QuickSort(arr, i + 1, right);
+        }
+    }
 }
 
-class BookList
+namespace StringOperations
 {
-    private List<string> books = new List<string>();
-    public void Add(string book) => books.Add(book);
-    public void Remove(string book) => books.Remove(book);
-    public bool Contains(string book) => books.Contains(book);
-    public string this[int index] => books[index];
+    public class StringTasks
+    {
+        public static int GetStringLength(string str)
+        {
+            int length = 0;
+            foreach (char c in str) length++;
+            return length;
+        }
+
+        public static void PrintReverse(string str)
+        {
+            for (int i = str.Length - 1; i >= 0; i--)
+                Console.Write(str[i]);
+            Console.WriteLine();
+        }
+
+        public static int CountWords(string str)
+        {
+            return str.Split(new[] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public static bool CompareStrings(string str1, string str2)
+        {
+            if (str1.Length != str2.Length) return false;
+            for (int i = 0; i < str1.Length; i++)
+                if (str1[i] != str2[i]) return false;
+            return true;
+        }
+
+        public static void CountCharacters(string str, out int letters, out int digits, out int special)
+        {
+            letters = digits = special = 0;
+            foreach (char c in str)
+            {
+                if (char.IsLetter(c)) letters++;
+                else if (char.IsDigit(c)) digits++;
+                else special++;
+            }
+        }
+
+        public static string CopyString(string str)
+        {
+            char[] copiedStr = new char[str.Length];
+            for (int i = 0; i < str.Length; i++)
+                copiedStr[i] = str[i];
+            return new string(copiedStr);
+        }
+
+        public static void CountVowelsConsonants(string str, out int vowels, out int consonants)
+        {
+            vowels = consonants = 0;
+            string vowelSet = "AEIOUaeiou";
+            foreach (char c in str)
+            {
+                if (char.IsLetter(c))
+                {
+                    if (vowelSet.Contains(c)) vowels++;
+                    else consonants++;
+                }
+            }
+        }
+
+        public static void SortStringArray(string[] arr)
+        {
+            Array.Sort(arr);
+        }
+    }
 }
 
 class Program
 {
     static void Main()
     {
-        try
-        {
-            string expression = Console.ReadLine();
-            Console.WriteLine(expression.Split('*').Select(int.Parse).Aggregate((a, b) => a * b));
-        }
-        catch { Console.WriteLine("Ошибка ввода"); }
+        BankSystem.BankAccount account = new BankSystem.BankAccount(BankSystem.AccountType.Savings, 5000);
+        account.Deposit(1500);
+        account.Withdraw(2000);
+        account.PrintInfo();
 
-        try
-        {
-            string input = Console.ReadLine();
-            Console.WriteLine(int.Parse(input));
-        }
-        catch { Console.WriteLine("Выход за пределы int"); }
+        int[] arr = { 5, 2, 9, 1, 5, 6 };
+        SortingAlgorithms.Sort.BubbleSort(arr);
+        Console.WriteLine("Bubble Sorted: " + string.Join(", ", arr));
 
-        int size = int.Parse(Console.ReadLine());
-        char symbol = Console.ReadKey().KeyChar;
-        for (int i = 0; i < size; i++)
-            Console.WriteLine(new string(symbol, size));
+        int[] arr2 = { 10, 7, 8, 9, 1, 5 };
+        SortingAlgorithms.Sort.QuickSort(arr2, 0, arr2.Length - 1);
+        Console.WriteLine("Quick Sorted: " + string.Join(", ", arr2));
 
-        int number = int.Parse(Console.ReadLine());
-        Console.WriteLine(number.ToString() == new string(number.ToString().Reverse().ToArray()));
+        string str = "Hello World!";
+        Console.WriteLine("String Length: " + StringOperations.StringTasks.GetStringLength(str));
 
-        try
-        {
-            int num = int.Parse(Console.ReadLine());
-            Console.WriteLine(Convert.ToString(num, 2));
-        }
-        catch { Console.WriteLine("Ошибка перевода"); }
+        Console.Write("Reversed String: ");
+        StringOperations.StringTasks.PrintReverse(str);
+
+        Console.WriteLine("Word Count: " + StringOperations.StringTasks.CountWords(str));
+
+        Console.WriteLine("Comparison (Hello, Hello): " + StringOperations.StringTasks.CompareStrings("Hello", "Hello"));
+        Console.WriteLine("Comparison (Hello, World): " + StringOperations.StringTasks.CompareStrings("Hello", "World"));
+
+        StringOperations.StringTasks.CountCharacters(str, out int letters, out int digits, out int special);
+        Console.WriteLine($"Letters: {letters}, Digits: {digits}, Special: {special}");
+
+        string copiedStr = StringOperations.StringTasks.CopyString(str);
+        Console.WriteLine("Copied String: " + copiedStr);
+
+        StringOperations.StringTasks.CountVowelsConsonants(str, out int vowels, out int consonants);
+        Console.WriteLine($"Vowels: {vowels}, Consonants: {consonants}");
+
+        string[] strArr = { "Banana", "Apple", "Cherry", "Mango" };
+        StringOperations.StringTasks.SortStringArray(strArr);
+        Console.WriteLine("Sorted Strings: " + string.Join(", ", strArr));
     }
 }
